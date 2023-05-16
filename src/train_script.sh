9fe4168e64a489c87988afa2b0a73d1db5b3e3bf
oalee <oalee@proton.me>
2023-05-16 11:47:29 +0000

    train script

---
+++ src/train_script.sh
@@ -0,0 +1,21 @@
+deepspeed  --include localhost:1,2,6,7 train.py \                                                        
+    --model_name_or_path '/data/weights/wllama' \
+    --data_path /data/datasets/fa.json \
+    --output_dir /data/weights/pwizard \
+    --num_train_epochs 1 \
+    --model_max_length 2048 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 100 \
+    --save_total_limit 6 \
+    --learning_rate 2e-5 \
+    --warmup_steps 2 \
+    --logging_steps 2 \
+    --lr_scheduler_type "cosine" \
+    --report_to "wandb" \
+    --gradient_checkpointing True \
+    --deepspeed configs/deepspeed_config.json \
+    --fp16 True
