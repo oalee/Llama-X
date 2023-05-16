2ac6a1c4cfc944cfa5af93787c119629d4da3e14
oalee <oalee@proton.me>
2023-05-16 11:46:12 +0000

    updates

--- src/generate.py
+++ src/generate.py
@@ -121,19 +121,15 @@ def main(
 
 def generate_prompt(instruction, input=None):
     if input:
-        return f"""Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
-
+        return f"""
 ### Instruction:
 {instruction}
-
-### Input:
 {input}
 
 ### Response:
 """
     else:
-        return f"""Below is an instruction that describes a task. Write a response that appropriately completes the request.
-
+        return f"""
 ### Instruction:
 {instruction}
 
